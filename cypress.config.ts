72fdf9ca5831bfd6379fc433fc538db404e1ac91
Amir Samson <amirhsamson93@gmail.com>
2025-12-26 13:07:33 +0330

    new changes and a Test file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      delete process.env.HTTP_PROXY;
+      delete process.env.HTTPS_PROXY;
+      return config;
     },
   },
-});
+
+  
+  // Force Chrome to not use any proxy
+  env: {
+    NO_PROXY: '*'
+  }
+})
--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,3 +1,10 @@
-it('to register', () =>{
-    cy.visit('https://todo.qacart.com/signup')
+it('should be able to register', () =>{
+    cy.visit('https://todo.qacart.com/signup');
+    cy.get('[data-testid="first-name"]').type('Amir')
+    cy.get('[data-testid="last-name"]').type('Amir')
+    cy.get('[data-testid="email"]').type('601@gmail.com')
+    cy.get('[data-testid="password"]').type('12345678Aa')
+    cy.get('[data-testid="confirm-password"]').type('12345678Aa')
+    cy.get('[data-testid="submit"]').click();
+    cy.get('[data-testid="welcome"]').should("be.visible");
 })
