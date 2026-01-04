011172305fb074aaaf588a4a30a753bfd11912a0
Amir Samson <amirhsamson93@gmail.com>
2026-01-04 17:32:02 +0330

    Added the 'Describe' inside todo.cy.ts file and added the beforeEach() so that all methods could run before each test in that and we have less code.

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -4,42 +4,48 @@ import NewTodoPage from "../Pages/NewTodoPage";
 import RegisterPage from "../Pages/RegisterPage";
 import TodoPage from "../Pages/todoPage";
 
+describe('to do the test cases', ()=>{
+
+    let user: User;
+    let registerPage: RegisterPage;
+    let newTodoPage: NewTodoPage;
+    let todoPage: TodoPage;
+
+    beforeEach(()=>{
+        user = new User(
+        'Testing Croc',
+        "On chain",
+        "d100@test.com",
+        '1234qwer'
+    );
+        newTodoPage = new NewTodoPage();
+        todoPage = new TodoPage()
+        registerPage = new RegisterPage()
+        registerPage.RegisterUsingApi(user).then((response) =>{
+            user.setToken(response.body.access_token)
+        });
+    })
+
 it('should be able to add a todo', ()=>{
     const user = new User(
         'Testing Croc',
         "On chain",
-        "a154@example.com",
+        "a155@example.com",
         '1234qwer'
     );
-    const registerPage = new RegisterPage()
     registerPage.RegisterUsingApi(user);
-    const newTodoPage = new NewTodoPage();
     newTodoPage.load();
     newTodoPage.addTodoItem("how to Cypress");
     newTodoPage.SubmitButton();
-    const todoPage = new TodoPage()
     todoPage.getTodoItems().should('contain.text', 'how to Cypress');
 });
 
-
-
-it('should be able to delete a todo', ()=>{
-    const user = new User(
-        'Testing Croc',
-        "On chain",
-        "b500@test.com",
-        '1234qwer'
-    );
-    const registerPage = new RegisterPage()
-    registerPage.RegisterUsingApi(user).then((response) =>{
-        user.setToken(response.body.access_token)
-        new TodoApi().addTodo(user)
-    });
-
-    const todoPage = new TodoPage()
+it.only('should be able to delete a todo', ()=>{
+    new TodoApi().addTodo(user)
     todoPage.load()
     todoPage.getTodoItems().should('contain.text', 'how to Cypress');
     todoPage.getDeleteButton()
     todoPage.getNoTodoIcon().should('be.visible')
     todoPage.getNoTodoIcon().should('contain.text', 'No Available Todos')
 });
+})
